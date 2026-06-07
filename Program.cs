9c7229628fc387e444f683a0fc7a14c80b62a4b4
Kubas <jakub.danek@academicschool.cz>
2026-06-07 12:42:03 +0200

    feat: create simple Read loop

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿while (true)
+{
+    Console.Write("Meteo >");
+    var input = CleanInput(Console.ReadLine());
+    if (input.Count == 0)
+        continue;
+}
+
+List<string> CleanInput(string? input)
+{
+    if (input == null)
+    {
+        return [];
+    }
+
+    return input.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+}
